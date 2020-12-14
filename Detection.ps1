936bc1e442a195463103e4cea3bea0aa58ca9170
Mark Hopper <mahopper@microsoft.com>
2020-12-14 16:16:48 -0600

    Add files via upload

---
+++ Detection.ps1
@@ -0,0 +1,13 @@
+﻿$result = systeminfo.exe | findstr KB4577586
+$path = 'HKLM:\SOFTWARE\Macromedia\FlashPlayerPepper'
+
+if ($result -and !(Test-Path $path))
+ {
+    
+    Write-Output "Found KB4577586 and Pepper is gone."
+    exit 0
+ }
+ else
+ {
+    exit 1
+ }
